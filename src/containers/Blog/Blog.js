c18fa3afbeb4626fd76f1dae506014d658dde229
Hiep Lam <hieplam@Hieps-MacBook-Pro.local>
2018-10-09 22:05:53 +0700

    using axios to fetch/post data from/to Firebase

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
-
+//import axios from 'axios';
+import axios from '../../axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: null
+    }
+    postSelectedHandler = (id) => {
+        //console.log(id);
+        this.setState({selectedPostId: id});
+    }
+    componentDidMount(){
+        axios.get("/posts").then(response => {
+            const updatedPosts = response.data.slice(0,4).map(post => {
+                return  {...post, author: "Hiep"};
+            });
+            this.setState({posts: updatedPosts});
+        }).catch(err => {
+            //console.log(err);
+            this.setState({error: true});
+        });
+    }
     render () {
+        let posts = <p style={{textAlign: "center"}}>Something went wrong</p>;
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author} 
+                    clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+        }
+        
         return (
             <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
+                <section className="Posts">{posts}</section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
